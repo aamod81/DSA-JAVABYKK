2da657d5de487bae8807abe2a33c21bbbc8e8e8a
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2026-06-12 09:29:54 +0530

    linearsearch

---
+++ src/com/aamod/searchinginArray.java
@@ -0,0 +1,11 @@
+package com.aamod;
+
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] roll ={1,2,3,4,5};
+        System.out.println(roll [3]);
+    }
+}
