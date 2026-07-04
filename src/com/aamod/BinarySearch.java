12fdcd9bc21b096784c4358ffa80527f040d1da5
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2026-07-04 17:03:38 +0530

    binarySearch

--- src/com/aamod/BinarySearch.java
+++ src/com/aamod/BinarySearch.java
@@ -2,6 +2,29 @@ package com.aamod;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] nums = {10, 15, 50, 19, 40, 44, 48, 49};
+        int [] arr = {1,2,3,4,5,6,7,8,9,10};
+        int target = 9;
+        int ans = binarySearch(arr,target);
+        System.out.println(ans);
     }
-}
+        static int binarySearch(int[] arr, int target) {
+
+            int start = 0;
+            int end = arr.length - 1;
+
+            while (start <= end) {
+
+                int mid = start + (end - start) / 2;
+
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
