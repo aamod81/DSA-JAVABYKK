64913dfee41afd8f6c5f658e3a1c220a1c026913
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2026-07-10 18:54:48 +0530

    InsertionShort

---
+++ src/com/aamod/InsertionShort.java
@@ -0,0 +1,42 @@
+package com.aamod;
+
+import java.util.Arrays;
+
+public class InsertionShort {
+    public static void main(String[] args) {
+        int[] arr ={4,5,6,7,1,2};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    static void selection(int[] arr){
+        for (int i=0;i<arr.length;i++){
+            int last = arr.length-i-1;
+            int maxIndex = getMaxIndex(arr,0,last);
+            swap(arr,maxIndex,last);
+        }
+    }
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+    private static int getMaxIndex(int[] arr,int start,int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }}
